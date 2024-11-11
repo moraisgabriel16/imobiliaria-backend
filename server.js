1758be3070239d169191732409d173ca4829ce3f
Gabriel <moraisgabriel16@gmail.com>
2024-11-11 14:12:28 -0300

    ajaustes

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // server.js
 const express = require('express');
 const cors = require('cors'); // Importe o CORS
-const app = express();
 const clienteRoutes = require('./routes/clientes');
 
-app.use(cors()); // Ative o CORS para permitir requisições de qualquer origem
+// Inicializa o aplicativo Express
+const app = express();
+
+// Configuração do CORS para permitir requisições de qualquer origem
+app.use(cors());
 app.use(express.json());
 app.use('/api', clienteRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-    
-});
+// Exporta o app como uma função do Vercel
+module.exports = app;
