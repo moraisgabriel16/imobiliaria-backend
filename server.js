8e2f9757e7225e250bc6936cd75469d41b4181a5
Gabriel <moraisgabriel16@gmail.com>
2024-11-13 13:15:08 -0300

    ajuaste server

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const mongoose = require('mongoose');
 // Inicializa o aplicativo Express
 const app = express();
 
+// Middleware de CORS para permitir requisições de qualquer origem
+// Adicionando configurações mais específicas de CORS se necessário
+app.use(cors({
+    origin: '*', // Alternativamente, pode restringir a 'https://imobiliaria-green.vercel.app' para mais segurança
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}));
+
+// Middleware para fazer o parsing do JSON no body
+app.use(express.json());
+
 // Conectar ao MongoDB Atlas usando a variável de ambiente
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -15,21 +27,21 @@ mongoose.connect(process.env.MONGODB_URI, {
     console.log('Conectado ao MongoDB');
 }).catch((error) => {
     console.error('Erro ao conectar ao MongoDB:', error);
+    // Isso vai garantir que o erro de conexão ao banco seja sempre tratado
 });
 
-// Configuração do CORS para permitir requisições de qualquer origem
-app.use(cors());
-app.use(express.json());
+// Definindo a rota para clientes
 app.use('/api', clienteRoutes);
 
 // Definindo a porta para rodar localmente
 const PORT = process.env.PORT || 5000;
 
+// Inicializar servidor (apenas se não estiver no ambiente serverless)
 if (require.main === module) {
     app.listen(PORT, () => {
         console.log(`Servidor rodando na porta ${PORT}`);
     });
 }
 
-// Exporta o app como uma função do Vercel
+// Exporta o app como uma função para o Vercel
 module.exports = app;
