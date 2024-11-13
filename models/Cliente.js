74fbc4b23ffd3ccb6704dae4317c6af14ad4fdb8
Gabriel <moraisgabriel16@gmail.com>
2024-11-13 13:34:45 -0300

    fix

--- models/Cliente.js
+++ models/Cliente.js
@@ -1,4 +1,3 @@
-// routes/clientes.js
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
@@ -6,7 +5,7 @@ const clientController = require('../controllers/clientController');
 router.get('/clientes', clientController.listarClientes);
 router.post('/clientes', clientController.cadastrarCliente);
 router.get('/clientes/:cpf', clientController.detalhesCliente);
-router.put('/clientes/:cpf', clientController.atualizarCliente);
-router.delete('/clientes/:cpf', clientController.excluirCliente);
+router.put('/clientes/:cpf', clientController.atualizarCliente); // Rota para atualizar cliente
+router.delete('/clientes/:cpf', clientController.excluirCliente); // Rota para excluir cliente
 
 module.exports = router;
